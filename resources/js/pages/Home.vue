f3ad456d925f4b824b3a877921008f209e9c5653
Michael <m.molin.1987@gmail.com>
2020-07-24 12:34:10 +0200

    Working vue

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container">
+        <h1>Vue-News-Meteo</h1>
+        <a href="https://newsapi.org/s/italy-news-api"> News Italy Api</a>
+        <br>
+        <a href="https://rapidapi.com/community/api/open-weather-map"> Weather app</a>
+    </div>
+</template>
